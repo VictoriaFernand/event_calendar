dc59c356fa576df73df0b8d677c1f094d5531a51
VictoriaFernand <118533025+VictoriaFernand@users.noreply.github.com>
2022-11-18 09:19:11 -0300

    calendariopage

---
+++ calendario.php
@@ -0,0 +1,167 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <link rel="icon" type="image/x-icon" href="images/2.png">
+    <link rel="stylesheet" href="css/sstyle.css"/>
+    <title>Calendario de Eventos</title>
+
+  </head>
+  <body>
+    <div class="event-container">
+      <h3 class="ano">2022</h3>
+
+      <div class="event">
+        <div class="event-left">
+          <div class="event-data">
+            <div class="data">17</div>
+            <div class="mes">Novembro</div>
+          </div>
+        </div>
+
+        <div class="event-right">
+          <h3 class="event-titulo">Some Title Here</h3>
+
+          <div class="event-descricao">
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Accusamus,
+            ratione.
+          </div>
+          <div class="event-timing">
+          <img src="images/de-volta-no-tempo.png" alt="" /> 10:45 am
+          </div>
+          <div class="event-view">
+        <a href="">view event</a>         
+        </div>
+        </div>
+      </div>
+
+      <div class="event">
+        <div class="event-left">
+          <div class="event-data">
+            <div class="data">01</div>
+            <div class="mes">Dezembro</div>
+          </div>
+        </div>
+
+        <div class="event-right">
+          <h3 class="event-titulo">Some Title Here</h3>
+
+          <div class="event-descricao">
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Accusamus,
+            ratione.
+          </div>
+
+           <div class="event-timing">
+          <img src="images/de-volta-no-tempo.png" alt="" /> 10:45 am
+          </div>
+          <div class="event-view">
+        <a href="">view event</a>         
+        </div>
+        </div>
+      </div>
+
+      <h3 class="year">2023</h3>
+
+      <div class="event">
+        <div class="event-left">
+          <div class="event-data">
+            <div class="data">13</div>
+            <div class="mes">Janeiro</div>
+          </div>
+        </div>
+
+        <div class="event-right">
+          <h3 class="event-titulo">Some Title Here</h3>
+
+          <div class="event-descricao">
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Accusamus,
+            ratione.
+          </div>
+
+          <div class="event-timing">
+          <img src="images/de-volta-no-tempo.png" alt="" /> 10:45 am
+          </div>
+          <div class="event-view">
+        <a href="">view event</a>         
+        </div>
+        </div>
+      </div>
+
+      <div class="event">
+        <div class="event-left">
+          <div class="event-data">
+            <div class="data">09</div>
+            <div class="mes">Março</div>
+          </div>
+        </div>
+
+        <div class="event-right">
+          <h3 class="event-titulo">Some Title Here</h3>
+
+          <div class="event-descricao">
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Accusamus,
+            ratione.
+          </div>
+
+           <div class="event-timing">
+          <img src="images/de-volta-no-tempo.png" alt="" /> 10:45 am
+          </div>
+          <div class="event-view">
+        <a href="">view event</a>         
+        </div>
+        </div>
+      </div>
+
+      <div class="event">
+        <div class="event-left">
+          <div class="event-data">
+            <div class="data">01</div>
+            <div class="mes">Abril</div>
+          </div>
+        </div>
+
+        <div class="event-right">
+          <h3 class="event-titulo">Some Title Here</h3>
+
+          <div class="event-descricao">
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Accusamus,
+            ratione.
+          </div>
+
+          <div class="event-timing">
+          <img src="images/de-volta-no-tempo.png" alt="" /> 10:45 am
+          </div>
+          <div class="event-view">
+        <a href="">view event</a>         
+        </div>
+        </div>
+      </div>
+
+      <div class="event">
+        <div class="event-left">
+          <div class="event-data">
+            <div class="data">08</div>
+            <div class="mes">Junho</div>
+          </div>
+        </div>
+
+        <div class="event-right">
+          <h3 class="event-titulo">Some Title Here</h3>
+
+          <div class="event-descricao">
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Accusamus,
+            ratione.
+          </div>
+
+          <div class="event-timing">
+          <img src="images/de-volta-no-tempo.png" alt="" /> 10:45 am
+          </div>
+          <div class="event-view">
+        <a href="">view event</a>         
+        </div>
+        </div>
+      </div>
+    </div>
+  </body>
+</html>
